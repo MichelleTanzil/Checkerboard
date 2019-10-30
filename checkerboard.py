b42d95e2686a139b3c332a358a388c5fc7e78132
Michelle Tanzil <mtanzil01@gmail.com>
2019-10-30 15:05:58 -0700

    fixed error where colors were not showing up right when the rows and cols were odd numbers

--- checkerboard.py
+++ checkerboard.py
@@ -6,19 +6,25 @@ app = Flask(__name__)
 def play():
     width = 600
     height = 600
-    return render_template("index.html", timesX=8, timesY=8, width=str(width) + "px", height=str(height) + "px")
+    color1 = "red"
+    color2 = "grey"
+    return render_template("index.html", timesX=8, timesY=8, width=str(width) + "px", height=str(height) + "px", color1 = color1, color2 = color2)
 
 @app.route("/<int:timesY>")
 def playX(timesY):
     width = 600
     height = timesY * 75
-    return render_template("index.html", timesX=8, timesY=timesY, width=str(width) + "px", height=str(height) + "px")
+    color1 = "red"
+    color2 = "grey"
+    return render_template("index.html", timesX=8, timesY=timesY, width=str(width) + "px", height=str(height) + "px", color1 = color1, color2 = color2)
 
 @app.route("/<int:timesX>/<int:timesY>")
 def playXY(timesX, timesY):
     width = timesX * 75
     height = timesY * 75
-    return render_template("index.html", timesX=timesX, timesY=timesY, width=str(width) + "px", height=str(height) + "px")
+    color1 = "red"
+    color2 = "grey"
+    return render_template("index.html", timesX=timesX, timesY=timesY, width=str(width) + "px", height=str(height) + "px", color1 = color1, color2 = color2)
 
 @app.route("/<int:timesX>/<int:timesY>/<color1>/<color2>")
 def playXYColor(timesX, timesY, color1, color2):
